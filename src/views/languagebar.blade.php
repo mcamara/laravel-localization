57e7df999c5cd2f30a76189be0363a79a60da23e
Marc Cámara <mcamara88@gmail.com>
2014-01-19 19:25:00 +0100

    hide not translated routes by default

--- src/views/languagebar.blade.php
+++ src/views/languagebar.blade.php
@@ -2,8 +2,8 @@
     @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
         @if($localeCode == LaravelLocalization::getCurrentLanguage())
             <li class="active">{{{ !empty($abbr) ? $localeCode : $properties['native'] }}}</li>
-        @else
-            <li><a rel="alternate" hreflang="{{$localeCode}}" href="{{LaravelLocalization::getLocalizedURL($localeCode)}}">{{{ !empty($abbr) ? $localeCode : $properties['native'] }}}</a></li>
+        @elseif($url = LaravelLocalization::getLocalizedURL($localeCode))
+            <li><a rel="alternate" hreflang="{{$localeCode}}" href="{{$url}}">{{{ !empty($abbr) ? $localeCode : $properties['native'] }}}</a></li>
         @endif
     @endforeach
 </ul>
