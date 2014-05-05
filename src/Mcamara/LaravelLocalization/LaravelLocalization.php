3bcb802dfaec0742fa8d8508d376b707d114e4ff
Ruud <ruud@inovamedia.nl>
2014-05-05 15:41:25 +0200

    fixed typo

--- src/Mcamara/LaravelLocalization/LaravelLocalization.php
+++ src/Mcamara/LaravelLocalization/LaravelLocalization.php
@@ -354,7 +354,7 @@ class LaravelLocalization
 			return false;
 		}
 
-		if (empty($transKeyNames))
+		if (empty($transKeysNames))
 		{
 			// if translation key name is not given
 			// the system would try to get the current one...
