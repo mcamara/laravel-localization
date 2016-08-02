359abeb667a79c30dff5c30ca43d769b92a15ea9
Mitchell <hipio@users.noreply.github.com>
2016-08-02 16:55:28 +0200

    Fix: Cookies now work
    I switched the if statements, fixed the redirect when the $locale is set and cookies are now set as they should. 
    
    You might also want to implement parts of this in the Session version as that one doesn't work either.

--- src/Mcamara/LaravelLocalization/Middleware/LocaleCookieRedirect.php
+++ src/Mcamara/LaravelLocalization/Middleware/LocaleCookieRedirect.php
@@ -12,26 +12,23 @@ class LocaleCookieRedirect {
      * @param  \Closure $next
      * @return mixed
      */
-    public function handle( $request, Closure $next )
-    {
-        $params = explode('/', $request->path());
-        $locale = $request->cookie('locale', false);
+    public function handle($request, Closure $next) {
+		$params = explode('/', $request->path());
+		$locale = $request->cookie('locale', false);
 
-        if ( count($params) > 0 && app('laravellocalization')->checkLocaleInSupportedLocales($params[ 0 ]) )
-        {
-            cookie('locale', $params[ 0 ]);
+		if ($locale !== false && (app('laravellocalization')->getDefaultLocale() === $locale && app('laravellocalization')->hideDefaultLocaleInURL())) {
+			$redirection = app('laravellocalization')->getLocalizedURL($locale);
+			$redirectResponse = new RedirectResponse($redirection, 302, ['Vary' => 'Accept-Language']);
 
-            return $next($request);
-        }
+			return $redirectResponse->withCookie(cookie()->forever('locale', $params[0]));
+		}
 
-        if ( $locale && !( app('laravellocalization')->getDefaultLocale() === $locale && app('laravellocalization')->hideDefaultLocaleInURL() ) )
-        {
-            app('session')->reflash();
-            $redirection = app('laravellocalization')->getLocalizedURL($locale);
+		if (count($params) > 0 && app('laravellocalization')->checkLocaleInSupportedLocales($params[0])) {
+			cookie('locale', $params[0]);
 
-            return new RedirectResponse($redirection, 302, [ 'Vary' => 'Accept-Language' ]);
-        }
+			return $next($request)->withCookie(cookie()->forever('locale', $params[0]));
+		}
 
-        return $next($request);
-    }
+		return $next($request);
+	}
 }
