2f001551760ae633055a4ab714e331531609ce36
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2016-12-20 09:06:51 +0000

    Apply fixes from StyleCI

--- src/Mcamara/LaravelLocalization/Exceptions/SupportedLocalesNotDefined.php
+++ src/Mcamara/LaravelLocalization/Exceptions/SupportedLocalesNotDefined.php
@@ -1,12 +1,13 @@
-<?php namespace Mcamara\LaravelLocalization\Exceptions;
+<?php
+
+namespace Mcamara\LaravelLocalization\Exceptions;
 
 use Exception;
 
-class SupportedLocalesNotDefined extends Exception {
-
-
+class SupportedLocalesNotDefined extends Exception
+{
     public function __construct()
     {
-        parent::__construct("Supported locales must be defined.");
+        parent::__construct('Supported locales must be defined.');
     }
 }
--- src/Mcamara/LaravelLocalization/Middleware/LocaleCookieRedirect.php
+++ src/Mcamara/LaravelLocalization/Middleware/LocaleCookieRedirect.php
@@ -1,32 +1,36 @@
-<?php namespace Mcamara\LaravelLocalization\Middleware;
+<?php
 
-use Illuminate\Http\RedirectResponse;
+namespace Mcamara\LaravelLocalization\Middleware;
+
 use Closure;
+use Illuminate\Http\RedirectResponse;
 
-class LocaleCookieRedirect {
-
+class LocaleCookieRedirect
+{
     /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  \Closure $next
-     * @return mixed
-     */
-     public function handle($request, Closure $next) {
-     	$params = explode('/', $request->path());
-	$locale = $request->cookie('locale', false);
-	
-	if (count($params) > 0 && app('laravellocalization')->checkLocaleInSupportedLocales($params[0])) {
-		return $next($request)->withCookie(cookie()->forever('locale', $params[0]));
-	}
-	
-	if ($locale && app('laravellocalization')->checkLocaleInSupportedLocales($locale) && !(app('laravellocalization')->getDefaultLocale() === $locale && app('laravellocalization')->hideDefaultLocaleInURL())) {
-		$redirection = app('laravellocalization')->getLocalizedURL($locale);
-		$redirectResponse = new RedirectResponse($redirection, 302, ['Vary' => 'Accept-Language']);
+      * Handle an incoming request.
+      *
+      * @param  \Illuminate\Http\Request $request
+      * @param  \Closure $next
+      *
+      * @return mixed
+      */
+     public function handle($request, Closure $next)
+     {
+         $params = explode('/', $request->path());
+         $locale = $request->cookie('locale', false);
 
-		return $redirectResponse->withCookie(cookie()->forever('locale', $params[0]));
-	}
-	
-	return $next($request);
+         if (count($params) > 0 && app('laravellocalization')->checkLocaleInSupportedLocales($params[0])) {
+             return $next($request)->withCookie(cookie()->forever('locale', $params[0]));
+         }
+
+         if ($locale && app('laravellocalization')->checkLocaleInSupportedLocales($locale) && !(app('laravellocalization')->getDefaultLocale() === $locale && app('laravellocalization')->hideDefaultLocaleInURL())) {
+             $redirection = app('laravellocalization')->getLocalizedURL($locale);
+             $redirectResponse = new RedirectResponse($redirection, 302, ['Vary' => 'Accept-Language']);
+
+             return $redirectResponse->withCookie(cookie()->forever('locale', $params[0]));
+         }
+
+         return $next($request);
      }
 }
