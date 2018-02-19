1054cd04c5a34488d71406ad53988207251111f1
Marc Cámara <mcamara88@gmail.com>
2018-02-19 11:20:33 +0100

    Revert "Update LaravelLocalization.php (#526)"
    This reverts commit 611b78e567debd3c0508675a7d5ddd65d03b488c.

--- src/Mcamara/LaravelLocalization/LaravelLocalization.php
+++ src/Mcamara/LaravelLocalization/LaravelLocalization.php
@@ -447,7 +447,6 @@ class LaravelLocalization
 
         switch ($this->getCurrentLocaleScript()) {
             // Other (historic) RTL scripts exist, but this list contains the only ones in current use.
-	    case 'Farsi':	
             case 'Arab':
             case 'Hebr':
             case 'Mong':
