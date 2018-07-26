21be3242c63baac7fa8b27bcd014602c37b31934
vladimir.reznichenko <vladimir.reznichenko@intellishop.ag>
2018-07-26 14:54:42 +0200

    Minor code tweaks

--- src/Mcamara/LaravelLocalization/LaravelLocalizationServiceProvider.php
+++ src/Mcamara/LaravelLocalization/LaravelLocalizationServiceProvider.php
@@ -7,13 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class LaravelLocalizationServiceProvider extends ServiceProvider
 {
     /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
-
-    /**
      * Bootstrap the application events.
      *
      * @return void
