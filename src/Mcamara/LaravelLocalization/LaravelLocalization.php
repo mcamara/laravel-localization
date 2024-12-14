739bca8e85e457f0fee11815cb022bd7ed686387
Adam Nielsen <info@drnielsen.de>
2024-12-14 14:39:30 +0100

    Remove setLocale, no longer used. Provide function to set locale for middleware.

--- src/Mcamara/LaravelLocalization/LaravelLocalization.php
+++ src/Mcamara/LaravelLocalization/LaravelLocalization.php
@@ -94,58 +94,6 @@ class LaravelLocalization
         }
     }
 
-    /**
-     * Set and return current locale.
-     *
-     * @param string $locale Locale to set the App to (optional)
-     *
-     * @return string Returns locale (if route has any) or null (if route does not have a locale)
-     */
-    public function setLocale($locale = null)
-    {
-        if (empty($locale) || !\is_string($locale)) {
-            // If the locale has not been passed through the function
-            // it tries to get it from the first segment of the url
-            $locale = $this->request->segment(1);
-        }
-
-        $locale = $this->getInversedLocaleFromMapping($locale);
-
-        if (!empty($this->supportedLocales[$locale])) {
-            $this->currentLocale = $locale;
-        } else {
-            // if the first segment/locale passed is not valid
-            // the system would ask which locale have to take
-            // it could be taken by the browser
-            // depending on your configuration
-
-            $locale = null;
-
-            // if we reached this point and hideDefaultLocaleInURL is true
-            // we have to assume we are routing to a defaultLocale route.
-            if ($this->hideDefaultLocaleInURL()) {
-                $this->currentLocale = $this->defaultLocale;
-            }
-            // but if hideDefaultLocaleInURL is false, we have
-            // to retrieve it from the browser...
-            else {
-                $this->currentLocale = $this->getCurrentLocale();
-            }
-        }
-
-        $this->app->setLocale($this->currentLocale);
-        $this->translator->setLocale($this->currentLocale);
-
-        // Regional locale such as de_DE, so formatLocalized works in Carbon
-        $regional = $this->getCurrentLocaleRegional();
-        $suffix = $this->configRepository->get('laravellocalization.utf8suffix');
-        if ($regional) {
-            setlocale(LC_TIME, $regional . $suffix);
-            setlocale(LC_MONETARY, $regional . $suffix);
-        }
-
-        return $this->getLocaleFromMapping($locale);
-    }
 
     /**
      * Check if $locale is default locale and supposed to be hidden in url
@@ -504,6 +452,10 @@ class LaravelLocalization
         return $this->supportedLocales[$this->getCurrentLocale()]['native'];
     }
 
+    public function setCurrentLocale(string $locale): void {
+        $this->currentLocale = $locale;
+    }
+
     /**
      * Returns current language.
      *
@@ -523,22 +475,6 @@ class LaravelLocalization
 
         // or get application default language
         return $this->configRepository->get('app.locale');
-    }
-
-    /**
-     * Returns current regional.
-     *
-     * @return string current regional
-     */
-    public function getCurrentLocaleRegional()
-    {
-        // need to check if it exists, since 'regional' has been added
-        // after version 1.0.11 and existing users will not have it
-        if (isset($this->supportedLocales[$this->getCurrentLocale()]['regional'])) {
-            return $this->supportedLocales[$this->getCurrentLocale()]['regional'];
-        } else {
-            return;
-        }
     }
 
     /**
